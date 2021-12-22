ab7317a358c533b5a47afef44483ac791db05b3d
laothomas01 <53920510+laothomas01@users.noreply.github.com>
2021-12-22 00:14:04 -0800

    finished candy children AI.

--- CandyGame/Assets/CandyChildren.cs
+++ CandyGame/Assets/CandyChildren.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class CandyChildren : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float aggroRange;
+    [SerializeField] float moveSpeed;
+    public float jumpInterval;
+    public float jumpForce;
+    private float time;
+    private float horizontal;
+    Rigidbody2D rb2d;
+    private Vector2 playerPosition;
+    [SerializeField] private float initWait;
+    private void Start()
     {
+        time = 0;
+        rb2d = GetComponent<Rigidbody2D>();
 
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        playerPosition = GameObject.FindWithTag("Player").transform.position;
+        float distToPlayer = Vector2.Distance(transform.position, playerPosition);
+        if (aggroRange > distToPlayer)
+        {
+            ChasePlayer();
+        }
     }
+    private void ChasePlayer()
+    {
+        jumpInterval = Random.Range(1, 3);
+        jumpForce = Random.Range(-1, 5);
+        moveSpeed = Random.Range(1, 5);
+        initWait += Time.deltaTime;
+        if (initWait > 5)
+        {
+            if (transform.position.x < playerPosition.x)
+            {
+                if (time > jumpInterval)
+                {
+
+                    rb2d.velocity = new Vector2(Vector2.right.x * moveSpeed, Vector2.up.y * jumpForce);
+                    time = 0;
+                }
+                else
+                {
+                    time += Time.deltaTime;
+                }
+            }
+            else
+            {
+                if (time > jumpInterval)
+                {
+                    horizontal = Random.RandomRange(1, 3);
+                    rb2d.velocity = new Vector2(-Vector2.right.x * moveSpeed, Vector2.up.y * jumpForce);
+                    time = 0;
+                }
+                else
+                {
+                    time += Time.deltaTime;
+                }
+            }
+        }
+    }
+    //void OnDrawGizmos()
+    //{
+
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(this.transform.position, aggroRange);
+    //}
 }
