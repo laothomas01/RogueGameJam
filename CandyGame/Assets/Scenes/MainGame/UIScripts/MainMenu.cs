3637a60b60e412e5850b7adc2842e3a242c2ba7f
laothomas01 <53920510+laothomas01@users.noreply.github.com>
2021-12-26 20:15:32 -0800

    finished start menu

--- CandyGame/Assets/Scenes/MainGame/UIScripts/MainMenu.cs
+++ CandyGame/Assets/Scenes/MainGame/UIScripts/MainMenu.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    //go to the first game scene.
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    //settings scene
     public void Settings()
     {
-        SceneManager.GetSceneByName("SettingsMenu");
+        SceneManager.LoadScene("SettingsMenu");
     }
+    //exit the game
     public void QuitGame()
     {
         Application.Quit();
--- CandyGame/Assets/Scenes/MainGame/UIScripts/Options.cs
+++ CandyGame/Assets/Scenes/MainGame/UIScripts/Options.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Options : MonoBehaviour
 {
-    private void Back()
+    public void Back()
     {
-
+        SceneManager.LoadScene("MainMenu");
     }
-       
 }
