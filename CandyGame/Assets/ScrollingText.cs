2dc75795dcc8bf2a6962acb7682b1ab753731910
Thomas Lao <laothomas01@gmail.com>
2021-12-31 13:03:58 -0800

    finished the prologue scene

--- CandyGame/Assets/ScrollingText.cs
+++ CandyGame/Assets/ScrollingText.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 
 public class ScrollingText : MonoBehaviour
 {
 
 
+
+
     public TextMeshProUGUI t;
+
     public float delay = 0.1f;
     private string message =
         "ENTRY LOG 1\n" +
@@ -17,19 +21,25 @@ public class ScrollingText : MonoBehaviour
         "FUSION CORE REACTOR STATUS: STABLE\n" +
         "SPECIMEN 24601 VITALS: STABLE\n" +
 
-        "WARNING: UNKNOWN SPECIMEN DETECTED\n" +
+        "WARNING: UNKNOWN NON-HUMAN SPECIMEN DETECTED\n" +
         "MERGING HUMAN DNA WITH SUGAR, CORN SYRUP, AND HYDROGENTATED PALM OIL\n"
         + "FUSION CORE OVERLOAD\n..........\n\n" +
 
-        "SYSTEM REBOOT HAS COMPLETED\n" +
-        "HELLO, MY NAME IS ZEGLER ROMANOV.I AM A QUANTUM PHYICIST FOR PROJECT RASPUTIN.\n" +
+        "SYSTEM REBOOT HAS COMPLETED.............." +
+         "\n.....\n"
+        + "\n.....\n"
+        + "\n.....\n"
+        + "\n.....\n";
+
+    private string message2 = "HELLO, MY NAME IS ZEGLER ROMANOV.\nI AM A QUANTUM PHYICIST FOR PROJECT RASPUTIN.\n" +
         "WE HAVE BEEN WORKING ON PROJECT RASPUTIN FOR 10 YEARS AND HAVE FINALLY PERFECTED THE QUANTUM TUNNELING.\n" +
         "BUT SOMETHING WAS NOT SUPPOSE TO ENTER THAT MACHINE AND SOMETHING WAS NOT SUPPOSE TO COME THROUGH THAT MACHINE. " +
         "IF YOU RECEIVE THIS MESSAGE, TURN OFF THE MACHINE!";
 
-
-
-
+    private string message3 = "GAME INPUTS:\n" +
+        "MOVEMENT:\n MOVE LEFT: A\nMOVE RIGHT: D\nJUMP: W\n" +
+        "SHOOT: LEFT MOUSE CLICK\n";
+    private string message4 = "             PRESS SPACE BAR TO CONTINUE             ";
 
 
 
@@ -38,6 +48,7 @@ public class ScrollingText : MonoBehaviour
     {
 
         StartCoroutine(ShowText());
+
 
     }
     IEnumerator ShowText()
@@ -48,12 +59,38 @@ public class ScrollingText : MonoBehaviour
             t.text = currText;
             yield return new WaitForSeconds(delay);
         }
+        for (int i = 0; i < message2.Length; i++)
+        {
+            currText = message2.Substring(0, i + 1);
+            t.text = currText;
+            yield return new WaitForSeconds(delay);
+        }
+        for (int i = 0; i < message3.Length; i++)
+        {
+            currText = message3.Substring(0, i + 1);
+            t.text = currText;
+            yield return new WaitForSeconds(delay);
+        }
+        for (int i = 0; i < message4.Length; i++)
+        {
+            currText = message4.Substring(0, i + 1);
+            t.text = currText;
+            yield return new WaitForSeconds(delay);
+        }
+
     }
     private void Update()
     {
-
-        //if(t)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Level1");
+        }
     }
+
+
+
+
+
     //// Start is called before the first frame update
     //void Start()
     //{
